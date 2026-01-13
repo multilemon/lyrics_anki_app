48720b8d67884ed2702f9cc97e3806fd878bd29e
multilemon <multi.blue.lemon@gmail.com>
2026-01-14 00:39:48 +0900

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2060927841"
+    serviceWorkerVersion: "1134919718"
   }
 });
