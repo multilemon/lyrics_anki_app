99bd1540e8207374386caa2a23721fde7439bda5
multilemon <multilemon@users.noreply.github.com>
2026-02-06 15:26:25 +0000

    deploy: edce40b8223327e931b614d6079fede3987554d4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,8 +8,8 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icons/Icon-192.png": "052f5e7831af233e53f5994d17ceff57",
 "icons/favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
 "manifest.json": "7169297c1e1ae7cc157d3e91639e4fc9",
-"index.html": "0498b7fa7f7f7e149886446185ab6a66",
-"/": "0498b7fa7f7f7e149886446185ab6a66",
+"index.html": "d81591e96451759d5e47b4913ce81396",
+"/": "d81591e96451759d5e47b4913ce81396",
 "index_prod.html": "ac661e037665253f01d0664dc56192b6",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -40,9 +40,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "052f5e7831af233e53f5994d17ceff57",
 "sqlite3.wasm": "9d4d8aa7522ca11c72eb148d1f23a3df",
-"flutter_bootstrap.js": "7f2880c390853f68866ff270a5cceddb",
+"flutter_bootstrap.js": "bd934ffee3a858dba46ccff6fe0343df",
 "version.json": "e18f919006e263e801a34fc8a1f563d3",
-"main.dart.js": "f3b210becd3c4c287c5255869835374e"};
+"main.dart.js": "8d9ddb10ce262adbdb25f752f5dbeda2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
